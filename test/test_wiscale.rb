acd3f44cf9a7d2a95f7d3be58b1f02eaf58775c1
Jon <jon@digital-drip.com>
2010-03-30 09:05:05 -0400

    Add simple tests for gem

--- test/test_wiscale.rb
+++ test/test_wiscale.rb
@@ -1,7 +1,17 @@
 require 'helper'
 
 class TestWiscaleRuby < Test::Unit::TestCase
-  should "probably rename this file and start testing for real" do
-    flunk "hey buddy, you should probably rename this file and start testing for real"
+  context "no login required" do
+    should "return 0 for connected" do
+      client = WiScale.new
+      assert(client.get_status == 0)
+    end
+  end
+
+  context "invalid login" do
+    should "fail to get last measurement" do
+      client = WiScale.new(:userid => '2384', :publickey => 'asdf')
+      assert_equal 2555, client.get_last_meas
+    end
   end
 end
